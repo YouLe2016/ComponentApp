d96171a26b1c091b7b4946b970a2cf171809cdcf
江心才子 <270628297@qq.com>
2019-05-05 17:14:42 +0800

    OkGo网络框架引入

--- app/src/main/java/com/wyl/componentapp/MyApplication.kt
+++ app/src/main/java/com/wyl/componentapp/MyApplication.kt
@@ -1,18 +1,18 @@
 package com.wyl.componentapp
 
 import android.app.Application
+import com.lzg.extend.OkGoInitializer
+import com.lzy.okgo.utils.OkLogger
 
 lateinit var instance: MyApplication
-
-const val isDebug = true
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         instance = this
-
+        OkGoInitializer.initOkGo(this)
+        OkLogger.debug(BuildConfig.LOG_DEBUG)
     }
-
 
 }
